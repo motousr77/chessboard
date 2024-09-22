a2fc5ba3c3e9020337daacf8f358b236c9a5daa2
Anatolii Aleksandroff <motousr77@gmail.com>
2024-09-22 17:12:53 +0300

    new changes

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# SWITCH to 22
+node: v22.9.0 
+yarn: v1.22.22
+
+# TRY THIS ONE
+IAM
---
+++ src/compose/nextgen.js
@@ -0,0 +1,28 @@
+import wb from './images/pieces/wb.png';
+import wk from './images/pieces/wk.png';
+import wn from './images/pieces/wn.png';
+import wp from './images/pieces/wp.png';
+import wq from './images/pieces/wq.png';
+import wr from './images/pieces/wr.png';
+
+import bb from './images/pieces/bb.png';
+import bk from './images/pieces/bk.png';
+import bn from './images/pieces/bn.png';
+import bp from './images/pieces/bp.png';
+import bq from './images/pieces/bq.png';
+import br from './images/pieces/br.png';
+
+const imap = {
+    wb: wb,
+    wk: wk,
+    wn: wn,
+    wp: wp,
+    wq: wq,
+    wr: wr,
+    bb: bb,
+    bk: bk,
+    bn: bn,
+    bp: bp,
+    bq: bq,
+    br: br,
+};
